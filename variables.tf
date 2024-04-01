7335951b16d5921f614b3891301e2b157c0e0260
soundn <soundnwankwo@gmail.com>
2024-04-01 23:36:33 +0100

    coding..

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "ami" {
-  default = "ami-0885b1f6bd170450c"  # Replace with your preferred AMI
+  default = "ami-0914547665e6a707c"  # Replace with your preferred AMI
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "key_name" {
-  default = "your-key-name"  # Replace with your existing key pair name
+  default = "newaws"  # Replace with your existing key pair name
 }
 
 variable "vpc_security_group_ids" {
   type = list(string)
-  default = ["sg-12345678"]  # Replace with your security group IDs
+  default = ["sg-0cf0343ab84d9ef1b"]  # Replace with your security group IDs
 }
